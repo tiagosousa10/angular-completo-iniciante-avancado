c39cbcf638a18cd6466330c9f4dc48c719469aa2
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-15 10:03:15 +0000

    create first component

---
+++ conceitos-basicos/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HelloWorldComponent } from './helloworld/helloworld.component';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HelloWorldComponent, MinhapaginaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'conceitos-basicos';
+}
