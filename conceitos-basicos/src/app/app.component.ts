30af892eec49a786d484efa20cc06b2bc643d361
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-15 13:34:07 +0000

    add item to table list

--- conceitos-basicos/src/app/app.component.ts
+++ conceitos-basicos/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 @Component({
-  selector: 'app-root',
-  imports: [CalculadoraComponent, ListaComprasComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [ListaComprasComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'conceitos-basicos';
+    title = 'conceitos-basicos';
 }
--- conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-lista-compras',
-  imports: [],
-  templateUrl: './lista-compras.component.html',
-  styleUrl: './lista-compras.component.scss'
+    selector: 'app-lista-compras',
+    imports: [FormsModule, CommonModule],
+    templateUrl: './lista-compras.component.html',
+    styleUrl: './lista-compras.component.scss',
 })
 export class ListaComprasComponent {
+    item: string = '';
+    lista: ItemLista[] = [];
 
+    adicionarItem() {
+        let itemLista = new ItemLista();
+        itemLista.nome = this.item;
+        itemLista.id = this.lista.length + 1;
+
+        this.lista.push(itemLista);
+
+        this.item = '';
+    }
 }
