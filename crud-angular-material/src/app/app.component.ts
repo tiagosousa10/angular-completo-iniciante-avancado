d9b1d3d32562b9b3965837f2e60adffe22c097e9
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-17 11:47:51 +0000

    first routes and cadastro & consultado components

---
+++ crud-angular-material/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'crud-angular-material';
+}
