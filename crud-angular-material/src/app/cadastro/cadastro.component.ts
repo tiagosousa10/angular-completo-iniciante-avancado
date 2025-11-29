47945aa4f3a3b9338b3cdbff55ea01c01d675c19
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-29 11:20:58 +0000

    get api for select component

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -6,11 +6,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrasilapiService } from '../brasilapi.service';
+import { Estado, Municipio } from '../brasilapi.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
@@ -23,6 +27,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     MatIconModule,
     MatButtonModule,
     NgxMaskDirective,
+    MatSelectModule,
+    CommonModule,
   ],
   providers: [provideNgxMask()],
   templateUrl: './cadastro.component.html',
@@ -32,11 +38,14 @@ export class CadastroComponent implements OnInit {
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
   snack: MatSnackBar = inject(MatSnackBar);
+  estados: Estado[] = [];
+  municipios: Municipio[] = [];
 
   constructor(
     private service: ClienteService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private brasilApiService: BrasilapiService
   ) {}
 
   ngOnInit() {
@@ -50,11 +59,17 @@ export class CadastroComponent implements OnInit {
         if (clienteEncontrado) {
           this.atualizando = true;
           this.cliente = clienteEncontrado;
+          if (this.cliente.uf) {
+            const event = { value: this.cliente.uf };
+            this.carregarMunicipios(event as MatSelectChange);
+          }
         } else {
           this.cliente = Cliente.newCliente();
         }
       }
     });
+
+    this.carregarUFs();
   }
 
   salvar() {
@@ -71,5 +86,22 @@ export class CadastroComponent implements OnInit {
 
   mostrarMensagem(mensagem: string) {
     this.snack.open(mensagem, 'OK');
+  }
+
+  carregarUFs() {
+    //observable | subscriber
+    this.brasilApiService.listarUFs().subscribe({
+      next: (listaEstados) => (this.estados = listaEstados),
+      error: (error) => console.log('Ocorreu um erro', error),
+    });
+    console.log('passou por aqui');
+  }
+
+  carregarMunicipios(event: MatSelectChange) {
+    const ufSelectionada = event.value;
+    this.brasilApiService.listarMunicipios(ufSelectionada).subscribe({
+      next: (listaMunicipios) => (this.municipios = listaMunicipios),
+      error: (erro) => console.log('Ocorreu um erro', erro),
+    });
   }
 }
