0f5dc365e951892e4eaeac042c394651a6f06a0a
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-30 19:34:58 +0000

    form group created

--- passeio-app/src/app/categorias/categoria/categoria.component.ts
+++ passeio-app/src/app/categorias/categoria/categoria.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-categoria',
   standalone: false,
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss'
+  styleUrl: './categoria.component.scss',
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar() {
+    console.log('valores digitados', this.camposForm.value);
+    console.log('esta valido?', this.camposForm.valid);
+  }
 }
