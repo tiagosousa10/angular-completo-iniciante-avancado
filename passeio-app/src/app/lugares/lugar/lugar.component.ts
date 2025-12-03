fbfc897840f169eb0916f57c217702d790de76f0
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-03 15:00:56 +0000

    fetch categorias

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+
 @Component({
   selector: 'app-lugar',
   standalone: false,
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss',
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit {
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor() {
+  constructor(private categoriaService: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
       localizacao: new FormControl('', Validators.required),
       urlFoto: new FormControl('', Validators.required),
       avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => {
+        // console.log(listaCategorias);
+        this.categorias = listaCategorias;
+      },
     });
   }
 
