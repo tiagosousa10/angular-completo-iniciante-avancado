681d10fc1221c1a4be6ff63401fe320d639ca4fa
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-03 14:44:29 +0000

    lugar component ,first styles

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 @Component({
   selector: 'app-lugar',
   standalone: false,
   templateUrl: './lugar.component.html',
-  styleUrl: './lugar.component.scss'
+  styleUrl: './lugar.component.scss',
 })
 export class LugarComponent {
+  camposForm: FormGroup;
+  categorias: Categoria[] = [];
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar() {
+    console.log('Valores', this.camposForm.value);
+  }
 }
--- passeio-app/src/app/lugares/lugares.module.ts
+++ passeio-app/src/app/lugares/lugares.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { LugaresRoutingModule } from './lugares-routing.module';
 import { LugarComponent } from './lugar/lugar.component';
 
-
 @NgModule({
-  declarations: [
-    LugarComponent
-  ],
-  imports: [
-    CommonModule,
-    LugaresRoutingModule
-  ]
+  declarations: [LugarComponent],
+  imports: [CommonModule, LugaresRoutingModule, ReactiveFormsModule],
 })
-export class LugaresModule { }
+export class LugaresModule {}
