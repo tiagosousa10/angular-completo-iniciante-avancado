9ed9d04467f42140406272a7a6bd792d5e2bd6d6
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-03 15:11:10 +0000

    create a new place and get all

--- passeio-app/src/app/lugares/lugar.service.ts
+++ passeio-app/src/app/lugares/lugar.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Lugar } from './lugar';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LugarService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  salvar(lugar: Lugar): Observable<Lugar> {
+    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+  }
+
+  obterTodos(): Observable<Lugar[]> {
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
 }
