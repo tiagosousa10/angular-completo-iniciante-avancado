7815e7b4dcad55c71d7252ca2153349b9ce29167
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-06 15:48:33 +0000

    implement galeria component and fetch categorias and lugares

--- passeio-app/src/app/categorias/categoria.service.ts
+++ passeio-app/src/app/categorias/categoria.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../../categorias/categoria';
+import { Lugar } from '../../lugares/lugar';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
   standalone: false,
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
 
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe((categorias) => {
+      console.log('🚀 ~ GaleriaComponent ~ ngOnInit ~ categorias:', categorias);
+      return (this.categoriasFiltro = categorias);
+    });
+
+    this.lugarService.obterTodos().subscribe((lugaresResposta) => {
+      console.log(
+        '🚀 ~ GaleriaComponent ~ ngOnInit ~ lugaresResposta:',
+        lugaresResposta
+      );
+      return (this.lugares = lugaresResposta);
+    });
+  }
 }
