d73f3be6db4f63a9784984a4ba44ef8b4fbd4c46
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-22 17:46:54 +0000

    produtos page

--- microfront-ends/produtos/src/app/app.ts
+++ microfront-ends/produtos/src/app/app.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+
+interface Produto {
+  nome: string;
+  valor: number;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('produtos');
+  produtos: Produto[] = [
+    {
+      nome: 'Camiseta Angular',
+      valor: 49.99,
+      imagem: 'https://picsum.photos/id/1/200/150',
+    },
+    {
+      nome: 'Caneca Programador',
+      valor: 79.99,
+      imagem: 'https://picsum.photos/id/10/200/150',
+    },
+    {
+      nome: 'Rato Gamer',
+      valor: 45.37,
+      imagem: 'https://picsum.photos/id/25/200/150',
+    },
+    {
+      nome: 'Teclado',
+      valor: 23.99,
+      imagem: 'https://picsum.photos/id/30/200/150',
+    },
+    {
+      nome: 'Headset',
+      valor: 12.99,
+      imagem: 'https://picsum.photos/id/40/200/150',
+    },
+  ];
 }
