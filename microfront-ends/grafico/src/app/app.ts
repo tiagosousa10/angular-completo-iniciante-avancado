d5f6d83e19d75ca9630eeedf5f6dce350a9e62bd
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-22 18:36:37 +0000

    create a chart for vendas

--- microfront-ends/grafico/src/app/app.ts
+++ microfront-ends/grafico/src/app/app.ts
@@ -1,11 +1,44 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('grafico');
+export class App implements OnInit {
+  ngOnInit(): void {
+    const dados = [
+      { categoria: 'Eletronicos', valor: 10 },
+      { categoria: 'Roupas', valor: 15 },
+      { categoria: 'Decoração', valor: 5 },
+      { categoria: 'Ferramentas', valor: 13 },
+      { categoria: 'Ferramentas', valor: 24 },
+    ];
+
+    const labels = dados.map((valor) => valor.categoria);
+    const valores = dados.map((valor) => valor.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Categorias Mais Vendidas',
+            data: valores,
+            backgroundColor: 'rgba(31,82,146,0.6',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 }
